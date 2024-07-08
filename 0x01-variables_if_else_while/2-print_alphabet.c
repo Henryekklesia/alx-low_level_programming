c877840a0d25652c851abf8ddefcfee98378324b
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-08 16:26:52 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	for (char x = 'a'; x <= 'z'; x++)
+		putchar(x);
+
+	for (char y = 'A'; y <= 'Z'; y++)
+		putchar(y);
+
+return (0);
+}
