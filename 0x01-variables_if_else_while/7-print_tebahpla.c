377b3d128fe81afc58b04ec6daf0fdf587fa3320
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-09 08:37:49 +0100

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	char alpha = 'z';
+
+		while (alpha >= 'a')
+		{
+			putchar(alpha);
+			alpha--;
+		}
+
+		putchar('\n');
+
+return (0);
+
+}
