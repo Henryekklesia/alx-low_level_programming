9abd2ccb7a2c38ab88254092e6d96bafb7dd90d2
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-09 05:53:25 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	printf("\n");
+
+return (0);
+
+}
