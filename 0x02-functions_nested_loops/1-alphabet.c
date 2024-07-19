b6201362553de3f340d52f03e39e938cff2b6034
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-19 07:51:18 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function name for the ptototype
+ * Return: returns void
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
