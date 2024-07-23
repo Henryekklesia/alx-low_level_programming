b470d50e7cc5b0e2fb5eedd90ab834d63632b82b
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-23 09:08:17 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,18 @@
-
 #ifndef DOG_H
 #define DOG_H
 
 /**
  * struct dog - a new type describing a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * @name: The name of the dog, type = char *
+ * @age: The age of the dog, type = float
+ * @owner: The owner of the dog, type = char *
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to set
- * @age: age to set
- * @owner: owner to set
- */
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
 
