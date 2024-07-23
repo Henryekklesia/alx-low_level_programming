248909da89680e2bc8f350f4b8d5d50d89ca778f
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-23 03:50:42 +0100

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _islower - function for this prototype
+ * _islower - function for this prototype
  * @c: the parameter of integer type
  * Return: 1 for lowercase letters, 0 for otherwise
  */
