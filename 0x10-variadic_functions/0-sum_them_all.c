8c925cb451b6c0d338dbf3a03a7caf7b665f5f85
Henryekklesia <ekklesiamedialive@gmail.com>
2024-07-26 05:57:42 +0100

    print all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-		return 0;
+		return (0);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
@@ -23,6 +23,6 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(args);
 
-	return sum;
+	return (sum);
 }
 
