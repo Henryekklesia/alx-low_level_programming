1277859f2261eba0ea5d9b3a5d7e97c8c5ebb008
Henryekklesia <ekklesiamedialive@gmail.com>
2024-08-03 06:30:23 +0100

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit
+ * @d: digit printed.
+ * Return: Always 0
+ */
+int print_last_digit(int d)
+{
+	int digit = d % 10;
+
+		digit = -digit;
+}
+_putchar(digit);
+return (digit);
