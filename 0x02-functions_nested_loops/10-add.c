b4260c6ae4acd8ac166160982dfa31301102e423
Henryekklesia <ekklesiamedialive@gmail.com>
2024-08-15 13:04:55 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int add - adds two integers and returns the result
+ * @a: First parameter
+ * @b: second parameter
+ * Return: the sum of two integers
+ */
+
+int add(int a, int a);
+{
+	int sum = a + b;
+	return sum;
+}
