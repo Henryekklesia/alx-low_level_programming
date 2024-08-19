40f106243ce982f7a4183c71263f39c937d0e367
Henryekklesia <ekklesiamedialive@gmail.com>
2024-08-19 20:14:12 +0100

    fibonaci number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,15 @@
 int main(void)
 {
 	int x;
-	unsigned long long a = 0;
-	unsigned long long b = 1;
+	unsigned long a = 0;
+	unsigned long b = 1;
 
 	for (x = 1; x <= 50; x++)
 	{
 
-	unsigned long long fibo = a + b;
+	unsigned long fibo = a + b;
 
-		printf("%llu, ", fibo);
+		printf("%lu, ", fibo);
 
 		a = b;
 		b = fibo;
