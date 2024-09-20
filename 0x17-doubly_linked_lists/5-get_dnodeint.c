b47e308ef8d931cb4afc1222c55162d82dd4c4d0
Henryekklesia <ekklesiamedialive@gmail.com>
2024-09-20 05:41:52 +0100

    completed task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @head: head of list.
+ * @index: position of node
+ *
+ * Return: dlistint_t node or null if node doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *cursor = head;
+	unsigned int i = 0;
+
+	while (cursor)
+	{
+		if (i == index)
+			return (cursor);
+		cursor = cursor->next;
+		i++;
+	}
+	return (0);
+}
